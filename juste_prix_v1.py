505e72f8ab210c9c6c77ce788d34d29a8e50c39e
Sanja <sanja@Skynet.be>
2022-02-17 15:56:54 +0100

    modification1

--- juste_prix_v1.py
+++ juste_prix_v1.py
@@ -2,16 +2,13 @@
 import random
 #affichage du message de bienvenue 
 print("Bienvenue au jeu, Le juste prix")
-print("le juste prix est entre 1 et 100")
-reponse=random.randint(1, 100)
 #affiche un nombre au hazard
-print("Nombre qu'il faut trouver", reponse)
+reponse=random.randint(1, 100)
 #demader au jouer un prix
 print("Entrer un chiffre entre 1 et 100")
 proposition = input()
 proposition = int(proposition)
-#verification de la proposition du joueur
-
+#verification de la proposition du joueur, tant que la proposition !=reponse
 while proposition != reponse:
     if proposition < reponse:
         print("C'est plus")
