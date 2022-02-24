9845d3fa2f5edd3fba86df60f52d8ab4929d12ac
Sanja <sanja@Skynet.be>
2022-02-24 23:54:24 +0100

    modif2_pour_v1

---
+++ juste_prix_v2.py
@@ -0,0 +1,25 @@
+import random
+print("Bienvenue au jeu! Le juste prix.")
+print("Le juste prix est entre 1 et 100.")
+#affiche un nombre au hazard
+juste_prix=random.randint(1, 100)
+proposition = 0
+limite = 10
+compteur=1
+#vérification de la proposition du joueur, tant que la proposition !=réponse
+while proposition != juste_prix and compteur <= limite :
+    print("Tentative ", compteur, " sur ", limite)
+    print("Tu dois entrer un nombre.")
+    proposition = input()
+    proposition = int(proposition)
+    if proposition < juste_prix:
+        print("C'est plus")
+    elif proposition > juste_prix:
+        print("C'est moins")
+    elif proposition == juste_prix:
+        break
+    compteur+=1
+if proposition != juste_prix:
+    print("Tu as perdu! Le jute prix était", juste_prix,"!")
+else:
+    print("Félicitations! Tu as trouvé le juste prix en", compteur, "tentatives!","C'était bien",juste_prix,"!")
