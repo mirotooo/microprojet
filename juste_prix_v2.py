a652edf675d0637f4f7b06ffc60e05323475965d
Sanja <sanja@Skynet.be>
2022-03-01 20:02:51 +0100

    mofif v2

--- juste_prix_v2.py
+++ juste_prix_v2.py
@@ -8,9 +8,8 @@ limite = 10
 compteur=1
 #vérification de la proposition du joueur, tant que la proposition !=réponse
 while proposition != juste_prix and compteur <= limite :
-    print("Tentative ", compteur, " sur ", limite)
-    print("Tu dois entrer un nombre.")
-    proposition = input()
+    print("Tentative", compteur,"sur", limite)
+    proposition = input("Tu dois entrer un nombre.")
     proposition = int(proposition)
     if proposition < juste_prix:
         print("C'est plus")
@@ -22,4 +21,4 @@ while proposition != juste_prix and compteur <= limite :
 if proposition != juste_prix:
     print("Tu as perdu! Le jute prix était", juste_prix,"!")
 else:
-    print("Félicitations! Tu as trouvé le juste prix en", compteur, "tentatives!","C'était bien",juste_prix,"!")
+    print("Félicitations! Tu as trouvé le juste prix en", compteur,"tentatives!","C'était bien",juste_prix,"!")
